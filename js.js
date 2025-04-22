c91b9e6f41102fcb90ed275600c47e1043cea913
Daneo07 <rhydiangeorge7@gmail.com>
2025-04-22 15:25:44 +0100

    Introduced new grid layout using JS, needs work

--- js.js
+++ js.js
@@ -1,13 +1,52 @@
 
-//Sets Variables 
-const container = document.getElementsByClassName('container');
-const rows = document.getElementsByClassName('gridrow');
-const cell = document.getElementsByClassName('cell');
 
+const mkGrid = function(size){
+    const gameScreen = document.querySelector('.game');
+    for(i=0;i < size; i++){
+        const column = document.createElement('div');
+        column.classList.add('column');
+        for(j=0;j<= size; j++){
+            const row = document.createElement('div')
+            row.classList.add('row');
+            row.style.border = '2px solid black'
+            row.innerText = (i * size)+ j;
+            column.appendChild(row)
+
+        }
+        gameScreen.appendChild(column);
+    }
+}
+
+
+mkGrid(16);
+
+console.log('hello')
+
+
+
+
+
+
+
+/*
 //Function to create grid
 function createGrid(){
     mkRow(16);
-    mkCOLUMNS(16);
+    mkColumns(16);
 }
 
 //Create Columns
+function mkColumns(numCols){
+    for(i=0;i<numCols;i++){
+        let col = document.createElement('div')
+        col.classList.add('column');
+    }
+}
+
+//Create Rows
+function mkRow(numRows){
+    let row = document.createElement('div')
+    row.classList.add('row')
+}
+    */
+
